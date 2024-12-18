5ffe7f0aee72de388b02061513a5ef9da44cd1ec
minhnem <dtc2154801030092@ictu.edu.vn>
2024-12-18 09:27:36 +0700

    Export excel

--- src/controller/supplier.ts
+++ src/controller/supplier.ts
@@ -17,26 +17,6 @@ const addNew = async (req: any, res: any) => {
     }
 }
 
-const getSuppliers = async (req: any, res: any) => {
-    const {pageSize, page} = req.query
-    try {
-        const skip = (page - 1) * pageSize
-        const suppliers = await SupplierModel.find({}).skip(skip).limit(pageSize)
-        const total = await SupplierModel.countDocuments()
-        res.status(200).json({
-            message: 'Danh sách nhà cung cấp.',
-            data: {
-                suppliers,
-                total
-            }
-        })
-    } catch (error: any) {
-        res.status(404).json({
-            message: error.message
-        })
-    }
-}
-
 const updateSupplier = async (req: any, res: any) => {
     const body = req.body
     const { id } = req.query
@@ -68,4 +48,112 @@ const deleteSupplier = async (req: any, res: any) => {
     }
 }
 
-export {addNew, getSuppliers, updateSupplier, deleteSupplier}
+const getSuppliers = async (req: any, res: any) => {
+    const {pageSize, page} = req.query
+    try {
+        const skip = (page - 1) * pageSize
+        const suppliers = await SupplierModel.find({}).skip(skip).limit(pageSize)
+        const total = await SupplierModel.countDocuments()
+        res.status(200).json({
+            message: 'Danh sách nhà cung cấp.',
+            data: {
+                suppliers,
+                total
+            }
+        })
+    } catch (error: any) {
+        res.status(404).json({
+            message: error.message
+        })
+    }
+}
+
+const getDatas =  async (req: any, res: any) => {
+    const body = req.body
+    const {start, end} = req.query
+    console.log(body);
+    
+    try {
+        const filter: any = {}
+        if(start && end) {
+            filter.createdAt = {
+                $gte: start,
+                $lte: end
+            }
+        }
+        const suppliers = await SupplierModel.find(filter)
+        const items: any =  []
+        if(body.length > 0) {
+            suppliers.forEach((item: any) => {
+                const value: any = {}
+                body.forEach((key: string) => {
+                    value[`${key}`] = `${item._doc[`${key}`] ?? ''}`
+                })
+                items.push(value)
+            })
+        }
+        
+        res.status(200).json({
+            message: 'Lấy dữ liệu thành công',
+            data: items
+        })
+    } catch (error: any) {
+        res.status(404).json({
+            message: error.message
+        })
+    }
+}
+
+const getForm = async (req: any, res: any)=> {
+    try {
+        const form = {
+            title: 'Supplier',
+            layout: 'horizontal',
+            labelCal: 6,
+            wrapperCol: 18,
+            formItems: [
+                {
+                    key: 'name',
+                    value: 'name',
+                    label: 'Supplier name',
+                },
+                {
+                    key: 'product',
+                    value: 'product',
+                    label: 'Product',
+                },
+                {
+                    key: 'categories',
+                    value: 'categories',
+                    label: 'Categories',
+                },
+                {
+                    key: 'price',
+                    value: 'price',
+                    label: 'Price',
+                },
+                {
+                    key: 'email',
+                    value: 'email',
+                    label: 'Email',
+                },
+                {
+                    key: 'contact',
+                    value: 'contact',
+                    label: 'Contact',
+                },
+            ]
+        }
+
+        res.status(200).json({
+            message: '',
+            data: form
+        })
+    } catch (error: any) {
+        res.status(404).json({
+            message: error.message
+        })
+    }
+}
+
+export {addNew, getSuppliers, updateSupplier, deleteSupplier, getForm, getDatas}
--- src/router/supplier.ts
+++ src/router/supplier.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
-import { addNew, deleteSupplier, getSuppliers, updateSupplier } from "../controller/supplier";
+import { addNew, deleteSupplier, getDatas, getForm, getSuppliers, updateSupplier } from "../controller/supplier";
 
 const router = Router()
 
+router.get('/', getSuppliers)
+router.get('/get-form', getForm)
+router.post('/get-export-data', getDatas)
 router.post('/add-new', addNew)
 router.put('/update', updateSupplier)
 router.delete('/delete', deleteSupplier)
-router.get('/', getSuppliers)
 
 export default router
